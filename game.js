5c424f766ce22c20c7761413d94fb4c3f1e7d9bd
Jayden Pearse <spartanatreyu@hotmail.com>
2014-04-27 16:48:09 +1000

    Enemy 2 Ai finished, medpacks working, hud started

--- game.js
+++ game.js
@@ -103,6 +103,7 @@ testStateOutside.prototype.preload = function()
 	game.load.image('player', 'assets/graphics/test-player.png');
 	game.load.image('enemy1', 'assets/graphics/enemy1.png');
 	game.load.image('enemy2', 'assets/graphics/enemy2.png');
+	game.load.image('medpack', 'assets/graphics/medpack.png');
 	game.load.spritesheet('swing-attack', 'assets/graphics/swing-attack.png',48,48);
 };
 
@@ -120,9 +121,6 @@ testStateOutside.prototype.create = function()
 
 	//Setup player
 	this.player = game.add.sprite(176, 96, 'player');
-	this.player.attackCountdown = 0;
-	this.player.stepInsideEntity = playerStep;
-	this.player.health = 400;
 	game.camera.follow(this.player);
 	
 	//setup the input
@@ -138,14 +136,28 @@ testStateOutside.prototype.create = function()
 	this.mapEntities.add(this.player);
 	this.mapEntities.player = this.player;
 	
-	this.map.createFromObjects('Object Layer 1', 15, 'enemy1', '', true, false, this.mapEntities);
+	this.map.createFromObjects('Object Layer 1', 12, 'medpack', '', true, false, this.mapEntities);
+	this.map.createFromObjects('Object Layer 1', 14, 'enemy1', '', true, false, this.mapEntities);
+	this.map.createFromObjects('Object Layer 1', 15, 'enemy2', '', true, false, this.mapEntities);
 
-	//console.log(this.mapEntities.children);
 	for (var i = 0; i < this.mapEntities.children.length; i++)
 	{
 		game.physics.enable(this.mapEntities.getAt(i),Phaser.Physics.ARCADE);
-		this.mapEntities.getAt(i).body.setSize(32, 32, 0, 32);
+		
 
+		if (this.mapEntities.getAt(i).key == 'player')
+		{
+			this.mapEntities.getAt(i).attackCountdown = 0;
+			this.mapEntities.getAt(i).stepInsideEntity = playerStep;
+			this.mapEntities.getAt(i).health = 400;
+			this.mapEntities.getAt(i).body.setSize(32, 32, 0, 32);
+		}
+		if (this.mapEntities.getAt(i).key == 'medpack')
+		{
+			this.mapEntities.getAt(i).stepInsideEntity = medpackStep;
+			this.mapEntities.getAt(i).body.setSize(32, 32, 0, 0);
+			this.mapEntities.getAt(i).health = 10;
+		}
 		if (this.mapEntities.getAt(i).key == 'enemy1')
 		{
 			this.mapEntities.getAt(i).state = 'waiting';
@@ -153,9 +165,30 @@ testStateOutside.prototype.create = function()
 			this.mapEntities.getAt(i).searchY = this.mapEntities.getAt(i).y;
 			this.mapEntities.getAt(i).stepInsideEntity = enemy1Step;
 			this.mapEntities.getAt(i).health = 20;
+			this.mapEntities.getAt(i).body.setSize(32, 32, 0, 32);
+		}
+
+		if (this.mapEntities.getAt(i).key == 'enemy2')
+		{
+			this.mapEntities.getAt(i).state = 'waiting';
+			this.mapEntities.getAt(i).searchX = this.mapEntities.getAt(i).x;
+			this.mapEntities.getAt(i).searchY = this.mapEntities.getAt(i).y;
+			this.mapEntities.getAt(i).jumpCountdown = 0;
+			this.mapEntities.getAt(i).jumpAngle = 0;
+			this.mapEntities.getAt(i).jumpX = 0;
+			this.mapEntities.getAt(i).jumpY = 0;
+			this.mapEntities.getAt(i).stepInsideEntity = enemy2Step;
+			this.mapEntities.getAt(i).health = 60;
+			this.mapEntities.getAt(i).body.setSize(32, 32, 0, 32);
 		}
 
 	}
+	this.displayText = game.add.text(20, 20, 'Arrows to move, \'Z\' to attack\nHealth: '+this.player.health, { font: '16px Courier', fill: '#FFFFFF', align: 'left' });
+	this.displayText.fixedToCamera = true;
+	this.displayText.cameraOffset.x = 20;
+    this.displayText.cameraOffset.y = 20;
+
+
 
 	//console.log(this.mapEntities);
 
@@ -166,25 +199,47 @@ testStateOutside.prototype.update = function()
 	game.physics.arcade.collide(this.mapEntities, this.mapEntities,function(left,right)
 	{
 		//player and enemy attacks
-		if(left.key === 'enemy1' && right.key === 'player')
+		if((left.key === 'enemy1' || left.key === 'enemy2' || left.key === 'enemy3') && right.key === 'player')
 		{
 			right.health--;
 		}
 
-		if(left.key === 'player' && right.key === 'enemy1')
+		if(left.key === 'player' && (right.key === 'enemy1' || right.key === 'enemy2' || right.key === 'enemy3'))
 		{
 			left.health--;
+		}
+
+		//player and health
+		if(left.key === 'medpack' && right.key === 'player')
+		{
+			right.health+= 100;
+			if (right.health > 400)
+			{
+				right.health = 400;
+			}
+			left.health = 0;
+		}
+
+		//player and health
+		if(left.key === 'player' && right.key === 'medpack')
+		{
+			left.health+= 100;
+			if (left.health > 400)
+			{
+				left.health = 400;
+			}
+			right.health = 0;
 		}
 	});
 	game.physics.arcade.overlap(this.mapEntities, this.mapEntities,function(left,right)
 	{
 		//enemy and player attacks
-		if(left.key === 'enemy1' && right.key === 'swing-attack')
+		if((left.key === 'enemy1' || left.key === 'enemy2' || left.key === 'enemy3') && right.key === 'swing-attack')
 		{
 			left.health--;
 		}
 
-		if(left.key === 'swing-attack' && right.key === 'enemy1')
+		if(left.key === 'swing-attack' && (right.key === 'enemy1' || right.key === 'enemy2' || right.key === 'enemy3'))
 		{
 			right.health--;
 		}
@@ -200,23 +255,29 @@ testStateOutside.prototype.update = function()
 
 	//Do the depth sort
 	this.mapEntities.sort('y', Phaser.Group.SORT_ASCENDING);
+
+	//update text
+	this.displayText.setText('Arrows to move, \'Z\' to attack\nHealth: '+this.player.health/4+'%');
+
 };
 
 
 testStateOutside.prototype.render = function()
 {
+	
 	/*for (var i = 0; i < this.mapEntities.children.length; i++)
 	{
 		game.debug.body(this.mapEntities.getAt(i));
 
-		//if (this.mapEntities.getAt(i).key == 'enemy1')
-		//{
-		//	game.debug.text(this.mapEntities.getAt(i).state, 100, 380 );
-		//}
+		if (this.mapEntities.getAt(i).key == 'enemy2')
+		{
+			game.debug.text(this.mapEntities.getAt(i).state, 100, 380 );
+		}
 	}/**/
 	//game.debug.cameraInfo(game.camera, 32, 32);
 	
-	game.debug.text(this.player.health, 30, 30 );
+	//game.debug.text('Arrows to move,\'Z\' to attack', 30, 30 );
+	//game.debug.text('Health: '+this.player.health, 30, 50 );
 };
 
 function playerStep()
@@ -367,7 +428,6 @@ function playerStep()
 
 	if (this.health < 1)
 	{
-		console.log('player dead');
 		this.destroy();
 	}
 }
@@ -380,6 +440,14 @@ function swingAttack()
 	}
 
 	this.healthValue--;
+}
+
+function medpackStep()
+{
+	if (this.health === 0)
+	{
+		this.destroy();
+	}
 }
 
 function enemy1Step()
@@ -427,7 +495,75 @@ function enemy1Step()
 
 	if (this.health < 1)
 	{
-		console.log('enemy1 dead');
+		this.destroy();
+	}
+}
+
+function enemy2Step()
+{
+	if (this.state == 'waiting')
+	{
+		//if target found
+		if (Phaser.Math.distance(this.parent.player.x,this.parent.player.y,this.x,this.y) < 300)
+		{
+			this.state = 'hunting';
+		}
+	}
+
+	if (this.state == 'searching')
+	{
+		//if target lost, search last known location
+		if (this.jumpCountdown === 0)
+		{
+			this.jumpAngle = game.math.angleBetween(this.x, this.y,this.searchX, this.searchY);
+			this.jumpCountdown = 50;
+
+			this.jumpX = Math.sin(this.jumpAngle) * 400;
+			this.jumpY = Math.cos(this.jumpAngle) * 400;
+		}
+
+		//if done searching
+		if (Phaser.Math.distance(this.x,this.y,this.searchX,this.searchY) < 10)
+		{
+			this.state = 'waiting';
+		}
+
+		//if target found
+		if (Phaser.Math.distance(this.parent.player.x,this.parent.player.y,this.x,this.y) < 300)
+		{
+			this.state = 'hunting';
+		}
+	}
+
+	if (this.state == 'hunting')
+	{
+		if (this.jumpCountdown < 1)
+		{
+			this.jumpAngle = game.math.angleBetween(this.x, this.y,this.parent.player.x, this.parent.player.y);
+			this.jumpCountdown = 50;
+
+			this.jumpX = Math.sin(this.jumpAngle) * 400;
+			this.jumpY = Math.cos(this.jumpAngle) * 400;
+		}
+
+		if (Phaser.Math.distance(this.parent.player.x,this.parent.player.y,this.x,this.y) > 350)
+		{
+			this.state = 'searching';
+			this.searchX = this.parent.player.x;
+			this.searchY = this.parent.player.y;
+		}
+	}
+
+	this.jumpX *= 0.9;
+	this.jumpY *= 0.9;
+
+	this.body.velocity.x = this.jumpX;
+	this.body.velocity.y = this.jumpY;
+
+	this.jumpCountdown--;
+
+	if (this.health < 1)
+	{
 		this.destroy();
 	}
 }
